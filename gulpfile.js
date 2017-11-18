04c2fecb4ce959174cfafe17dab21fb085b7dc10
Pavel Mukha <olddex@gmail.com>
2017-11-18 10:37:53 +0200

    добавлена планшетная адаптивная версия, добавлены вендорные префиксы

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('sass', function () {
     gulp.src("./sass/style.scss")
@@ -10,3 +12,13 @@ gulp.task('sass', function () {
 gulp.task("sass:watch", function () {
    gulp.watch("./sass/**/*.scss", ['sass']);
 });
+
+gulp.task('default', () =>
+gulp.src('css/style.css')
+    .pipe(autoprefixer({
+        browsers: ['last 1000 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('dist'))
+);
+
